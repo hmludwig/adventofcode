b11b34fa7e12a2e1232cf307eba931e63a80d2d7
Harald Ludwig <23039398+hmludwig@users.noreply.github.com>
2021-09-01 12:14:59 +0200

    Fixed buggs in 2020 Day21

--- 2020/src/day21.py
+++ 2020/src/day21.py
@@ -1,43 +1,43 @@
 import sys
-from collections import defaultdict
-
 
 f = open(sys.argv[1])
 data = f.read().strip().splitlines()
 data = [x.split('(contains ') for x in data]
-data = [(sorted(x.split()), y.replace(')','').replace(',','').split()) for x,y in data]
+data = [(sorted(x.split()), y.replace(')', '').replace(',', '').split())
+        for x, y in data]
 
-ingredients = defaultdict(lambda: 0)
-for _, y in data:
-    for x in y:
-        ingredients[x] += 1
+allergen_wiki = {}
+all_ingredients = []
+for foods, allergens in data:
+    all_ingredients.extend(foods)
+    for allergen in allergens:
+        if allergen in allergen_wiki:
+            allergen_wiki[allergen].intersection_update(set(foods))
+        else:
+            allergen_wiki[allergen] = set(foods)
 
-ingredients = sorted(ingredients.items(), key=lambda k_v: k_v[1], reverse=True)
-
-dangerous_ingredients = []
-
-for key, value in ingredients:
-    tmp = []
-    for x, y in data:
-        if key in y:
-            tmp.append(x)
-    common = list(set.intersection(*map(set, tmp)))
-    if common:
-        dangerous_ingredients.append((common[0], key))
-    for x, y in data:
-        if common:
-            if common[0] in x:
-                x.remove(common[0])
+tmp = []
+for key in allergen_wiki:
+    tmp.append(allergen_wiki[key])
+known_ingredients = set.union(*tmp)
 
 solution1 = 0
-for x, y in data:
-    solution1 += len(x)
-
-dangerous_ingredients =  sorted(dangerous_ingredients, key=lambda x: x[1])
-
-solution2 = ''
-for x,_ in dangerous_ingredients:
-    solution2 += x + ','
+for x in all_ingredients:
+    if x not in known_ingredients:
+        solution1 += 1
 
 print(f'Solution 1: {solution1}')
+
+while sum([len(x) for x in allergen_wiki.values()]) != len(allergen_wiki):
+    allergen_wiki = dict(sorted(allergen_wiki.items(), key = lambda x : len(x[1])))
+    for key1 in allergen_wiki:
+        if len(allergen_wiki[key1]) == 1:
+            for key2 in allergen_wiki:
+                if key1 != key2:
+                    allergen_wiki[key2] -=  allergen_wiki[key1]
+
+solution2 = ''
+for key in sorted(allergen_wiki):
+    solution2 += str(list(allergen_wiki[key])[0]) + ','
+
 print(f'Solution 2: {solution2[:-1]}')
